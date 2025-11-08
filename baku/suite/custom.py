2c5458dcac97fb5828b2d9dda5da73f594713d03
pianapolataa <sissi.hu@hotmail.com>
2025-11-07 21:53:12 -0500

    .

--- baku/suite/custom.py
+++ baku/suite/custom.py
@@ -1,14 +1,108 @@
+# from dataclasses import dataclass, field
+# import numpy as np
+# import hydra
+# from omegaconf import DictConfig
+
+# class ObsArray:
+#     def __init__(self, shape, dtype):
+#         self.shape = shape
+#         self.dtype = dtype
+
+# # Minimal dummy environment for testing with preprocessed data
+# class DummyEnv:
+#     def __init__(self, state_dim, action_dim, max_episode_len, image_shape=(84, 84, 3), **kwargs):
+#         self._state_dim = state_dim
+#         self._action_dim = action_dim
+#         self._max_episode_len = max_episode_len
+#         self._image_shape = image_shape
+#         self.current_step = 0
+    
+#     def observation_spec(self):
+#         return {
+#             "pixels0": ObsArray(self._image_shape, np.uint8),
+#             "features": ObsArray((self._state_dim,), np.float32),
+#         }
+
+#     def action_spec(self):
+#         return np.zeros((self._action_dim,), dtype=np.float32)
+
+#     def reset(self):
+#         self.current_step = 0
+#         return {
+#             "pixels0": np.zeros(self._image_shape, dtype=np.uint8),
+#             "features": np.zeros((self._state_dim,), dtype=np.float32),
+#         }
+
+#     def step(self, action):
+#         self.current_step += 1
+#         done = self.current_step >= self._max_episode_len
+#         return DummyTimeStep(done, self._state_dim, self._image_shape)
+
+# class DummyTimeStep:
+#     def __init__(self, done, state_dim, image_shape):
+#         self.last_flag = done
+#         self.reward = 0.0
+#         self.observation = {
+#             "pixels0": np.zeros(image_shape, dtype=np.uint8),
+#             "features": np.zeros((state_dim,), dtype=np.float32),
+#             "goal_achieved": False,
+#         }
+
+#     def last(self):
+#         return self.last_flag
+
+# # Flexible task creation function
+# def make_custom_task(dataset, env_cls=DummyEnv, **env_kwargs):
+#     """
+#     Returns a list of envs and task descriptions.
+#     env_cls: either DummyEnv (for dry run) or your real env
+#     env_kwargs: parameters to pass to env_cls constructor
+#     """
+#     # If dataset is a config, instantiate it
+#     if isinstance(dataset, (dict, DictConfig)):
+#         dataset = hydra.utils.call(dataset)
+
+#     envs = [env_cls(dataset._max_state_dim, dataset._max_action_dim, dataset._max_episode_len, **env_kwargs)]
+#     task_descriptions = [dataset.task_emb]
+#     return envs, task_descriptions
+
+# def task_make_fn(dataset, env_cls=DummyEnv, max_episode_len=1000, max_state_dim=50, max_action_dim=10, **env_kwargs):
+#     if isinstance(dataset, (dict, DictConfig)):
+#         dataset = hydra.utils.call(dataset)
+
+#     # don't forward config-only keys to env constructor
+#     env_kwargs = dict(env_kwargs)
+#     env_kwargs.pop("max_action_dim", None)
+
+#     state_dim = getattr(dataset, "_max_state_dim", max_state_dim)
+#     action_dim = getattr(dataset, "_max_action_dim", None)
+#     episode_len = getattr(dataset, "_max_episode_len", max_episode_len)
+
+#     envs = [env_cls(state_dim, action_dim, episode_len, **env_kwargs)]
+#     task_descriptions = [getattr(dataset, "task_emb", None)]
+#     return envs, task_descriptions
+
+# @dataclass
+# class CustomSuite:
+#     dataset: any
+#     env_cls: type = DummyEnv
+#     hidden_dim: int = 256
+#     action_repeat: int = 1
+#     discount: float = 0.99
+#     num_eval_episodes: int = 1
+#     pixel_keys: list = field(default_factory=list)
+#     proprio_key: str = "features"
+#     feature_key: str = "features"
+    
+#     # Add task_make_fn so OmegaConf can see it
+#     task_make_fn: any = field(default_factory=lambda: task_make_fn)
+
 from dataclasses import dataclass, field
 import numpy as np
 import hydra
 from omegaconf import DictConfig
 
-class ObsArray:
-    def __init__(self, shape, dtype):
-        self.shape = shape
-        self.dtype = dtype
-
-# Minimal dummy environment for testing with preprocessed data
+# Minimal dummy environment compatible with BCAgent
 class DummyEnv:
     def __init__(self, state_dim, action_dim, max_episode_len, image_shape=(84, 84, 3), **kwargs):
         self._state_dim = state_dim
@@ -16,11 +110,12 @@ class DummyEnv:
         self._max_episode_len = max_episode_len
         self._image_shape = image_shape
         self.current_step = 0
-    
+
     def observation_spec(self):
+        # Return actual numpy arrays for the agent
         return {
-            "pixels0": ObsArray(self._image_shape, np.uint8),
-            "features": ObsArray((self._state_dim,), np.float32),
+            "pixels0": np.zeros(self._image_shape, dtype=np.uint8),
+            "features": np.zeros((self._state_dim,), dtype=np.float32),
         }
 
     def action_spec(self):
@@ -52,30 +147,15 @@ class DummyTimeStep:
         return self.last_flag
 
 # Flexible task creation function
-def make_custom_task(dataset, env_cls=DummyEnv, **env_kwargs):
-    """
-    Returns a list of envs and task descriptions.
-    env_cls: either DummyEnv (for dry run) or your real env
-    env_kwargs: parameters to pass to env_cls constructor
-    """
-    # If dataset is a config, instantiate it
-    if isinstance(dataset, (dict, DictConfig)):
-        dataset = hydra.utils.call(dataset)
-
-    envs = [env_cls(dataset._max_state_dim, dataset._max_action_dim, dataset._max_episode_len, **env_kwargs)]
-    task_descriptions = [dataset.task_emb]
-    return envs, task_descriptions
-
 def task_make_fn(dataset, env_cls=DummyEnv, max_episode_len=1000, max_state_dim=50, max_action_dim=10, **env_kwargs):
     if isinstance(dataset, (dict, DictConfig)):
         dataset = hydra.utils.call(dataset)
 
-    # don't forward config-only keys to env constructor
     env_kwargs = dict(env_kwargs)
     env_kwargs.pop("max_action_dim", None)
 
     state_dim = getattr(dataset, "_max_state_dim", max_state_dim)
-    action_dim = getattr(dataset, "_max_action_dim", None)
+    action_dim = getattr(dataset, "_max_action_dim", max_action_dim)
     episode_len = getattr(dataset, "_max_episode_len", max_episode_len)
 
     envs = [env_cls(state_dim, action_dim, episode_len, **env_kwargs)]
@@ -90,9 +170,9 @@ class CustomSuite:
     action_repeat: int = 1
     discount: float = 0.99
     num_eval_episodes: int = 1
-    pixel_keys: list = field(default_factory=list)
+    pixel_keys: list = field(default_factory=lambda: ["pixels0"])
     proprio_key: str = "features"
     feature_key: str = "features"
     
-    # Add task_make_fn so OmegaConf can see it
+    # Make OmegaConf happy
     task_make_fn: any = field(default_factory=lambda: task_make_fn)
