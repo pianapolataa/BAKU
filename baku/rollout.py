eaf319174072deabe9d4baffdcf0cf8f4fd8b130
pianapolataa <sissi.hu@hotmail.com>
2025-11-24 17:28:08 -0500

    ‘.’

--- baku/rollout.py
+++ baku/rollout.py
@@ -185,8 +185,8 @@ class AgentRollout:
                 # --- Always store for plotting ---
                 self.logged_data.append({
                     "timestamp": time.time() - t0,
-                    "action": feat.copy(),
-                    "action_1": feat_1.copy()
+                    "action": action.copy(),
+                    "action_1": action_1.copy()
                 })
 
                 elapsed = time.time() - t0
