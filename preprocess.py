61401d484e0c6b55346c18ab9f0e90af094e97c6
pianapolataa <sissi.hu@hotmail.com>
2025-11-24 16:19:40 -0500

    ‘.’

--- preprocess.py
+++ preprocess.py
@@ -29,7 +29,7 @@ def extract_quat(x):
 # GATHER ALL DEMO FOLDERS
 # ----------------------------
 demo_dirs = sorted(
-    [p for p in DATA_ROOT.iterdir() if p.is_dir() and "demonstration_14" in p.name]
+    [p for p in DATA_ROOT.iterdir() if p.is_dir() and "demonstration_12" in p.name]
 )
 print(f"Found {len(demo_dirs)} demos")
 
