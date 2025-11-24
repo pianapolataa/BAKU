c5a216b0162a4e844bc056c1f31493247ccd884d
pianapolataa <sissi.hu@hotmail.com>
2025-11-24 17:55:04 -0500

    ‘.’

--- baku/rollout.py
+++ baku/rollout.py
@@ -245,7 +245,7 @@ from omegaconf import DictConfig
 def main(cfg: DictConfig):
     demo_data_path = "/home_shared/grail_sissi/BAKU/processed_data_pkl/demo_task.pkl"
     # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.19_train/deterministic/193903/snapshot/57000.pt" # 3 demo policy best:57000
-    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.24_train/deterministic/170129/snapshot/39000.pt" # new 11 demo w noise
+    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.24_train/deterministic/174342/snapshot/28000.pt" # new 11 demo w noise
 
     rollout = AgentRollout(cfg, demo_data_path, snapshot_path, save_log=True)
     rollout.run(duration_s=180, freq=50)
