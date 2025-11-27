258c5f6136b305dafb980d934d68cfdcc04680cc
pianapolataa <sissi.hu@hotmail.com>
2025-11-27 15:59:45 -0500

    ‘.’

--- baku/rollout.py
+++ baku/rollout.py
@@ -143,7 +143,7 @@ class AgentRollout:
                 cnt += 1
                 arm_state = self.get_arm_state()
                 ruka_state = self.handler.hand.read_pos()
-                demo_obs = self.demo_data["observations"][min(cnt+1, len(self.demo_data["observations"]) - 1)]
+                demo_obs = self.demo_data["observations"][min(cnt, len(self.demo_data["observations"]) - 1)]
                 arm_state_1 = demo_obs["arm_states"].copy()
                 ruka_state_1 = demo_obs["ruka_states"].copy()
 
@@ -179,7 +179,7 @@ class AgentRollout:
                 obs_1 = {
                     "features": feat_1,
                     # "pixels0": np.zeros((3, 84, 84), dtype=np.uint8),
-                    "pixels0": self.demo_data["observations"][min(cnt+1, len(self.demo_data["observations"]) - 1)]["pixels0"],
+                    "pixels0": self.demo_data["observations"][min(cnt, len(self.demo_data["observations"]) - 1)]["pixels0"],
                     "task_emb": np.asarray(self.demo_data["task_emb"], dtype=np.float32),
                 }
 
@@ -200,7 +200,7 @@ class AgentRollout:
                     "action_1": action_1.copy()
                 })
                 
-                if (cnt < 200): action = action_1.copy()
+                if (cnt < 2): action = action_1.copy()
                 print(cnt)
 
                 arm_action = self.norm_quat_vec(action[:7])
