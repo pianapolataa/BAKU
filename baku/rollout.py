ebb0aa2ea0b3d453f24df16a24a3c253165f0da5
pianapolataa <sissi.hu@hotmail.com>
2025-11-27 13:57:29 -0500

    ‘.’

--- baku/rollout.py
+++ baku/rollout.py
@@ -200,7 +200,7 @@ class AgentRollout:
                     "action_1": action_1.copy()
                 })
                 
-                if (cnt < 200): action = action_1.copy()
+                if (cnt < 20): action = action_1.copy()
                 print(cnt)
 
                 arm_action = self.norm_quat_vec(action[:7])
@@ -283,7 +283,7 @@ from omegaconf import DictConfig
 def main(cfg: DictConfig):
     demo_data_path = "/home_shared/grail_sissi/BAKU/processed_data_pkl/demo_task.pkl"
     # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.24_train/deterministic/174342/snapshot/86000.pt" # WORKING NONVISUAL POLICY
-    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.27_train/deterministic/133542/snapshot/21000.pt" # visual bc
+    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.11.27_train/deterministic/133542/snapshot/22000.pt" # visual bc
 
     rollout = AgentRollout(cfg, demo_data_path, snapshot_path, save_log=True)
     rollout.run(duration_s=180, freq=50)
