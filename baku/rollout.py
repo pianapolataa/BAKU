805435480ec3209155ac8b1a677ecdff08d63c7f
pianapolataa <sissi.hu@hotmail.com>
2025-12-24 15:59:22 -0500

    .

--- baku/rollout.py
+++ baku/rollout.py
@@ -204,7 +204,7 @@ class AgentRollout:
                     "action_1": action_1.copy()
                 })
                 
-                if (cnt < 2): action = action_1.copy()
+                if (cnt < 20): action = action_1.copy()
                 print(cnt)
 
                 arm_action = self.norm_quat_vec(action[:7])
