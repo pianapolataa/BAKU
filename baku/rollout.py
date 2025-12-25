4050de731bebbadf5f834f24849a27a2630c954e
pianapolataa <sissi.hu@hotmail.com>
2025-12-25 14:32:41 -0500

    revert

--- baku/rollout.py
+++ baku/rollout.py
@@ -473,7 +473,7 @@ class AgentRollout:
 
                 obs_demo = {
                     "features": feat_demo,
-                    "pixels0": demo_obs["pixels0"].copy(), # same shape
+                    "pixels0": rgb.copy(), # same shape
                     "task_emb": task_emb
                 }
 
@@ -490,15 +490,16 @@ class AgentRollout:
                     if isinstance(action_demo, torch.Tensor):
                         action_demo = action_demo.cpu().numpy()
 
-                if (cnt < 2):
-                    action = action_demo.copy()
-
                 # Logging
                 self.logged_data.append({
                     "timestamp": time.time() - t0,
                     "action": action.copy(),
                     "action_demo": action_demo.copy()
                 })
+
+                # Use demo first step to stabilize
+                if cnt < 2:
+                    action = action_demo.copy()
 
                 # Apply to Franka & Ruka
                 arm_action = self.norm_quat_vec(action[:7])
@@ -540,8 +541,8 @@ from omegaconf import DictConfig
 def main(cfg: DictConfig):
     demo_data_path = "/home_shared/grail_sissi/BAKU/processed_data_pkl/demo_task.pkl"
     # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.03_train/deterministic/140501/snapshot/53000.pt" # working variation bread pickup visual bc
-    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.25_train/deterministic/105414/snapshot/43000.pt" # working moving music box
-    # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.25_train/deterministic/132413/snapshot/47000.pt" # new moving music box
+    # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.24_train/deterministic/215958/snapshot/23000.pt" # working music box
+    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.25_train/deterministic/105414/snapshot/43000.pt" # moving music box
 
     rollout = AgentRollout(cfg, demo_data_path, snapshot_path, save_log=True)
     rollout.run(duration_s=180, freq=50)
