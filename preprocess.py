5ff4b67a6743260fd8103239f49bf07a77e5bde9
pianapolataa <sissi.hu@hotmail.com>
2026-01-14 20:34:01 -0500

    .

--- preprocess.py
+++ preprocess.py
@@ -56,6 +56,7 @@ fixed_counts = {"global_flips": 0, "continuity_flips": 0}
 for d_idx, DEMO in enumerate(demo_dirs):
     print(f"\n=== Processing {DEMO.name} ===")
     if DEMO.name == "demonstration_127": continue
+    if DEMO.name == "demonstration_102": continue
 
     # Load pkl data
     with open(DEMO / "states.pkl", "rb") as f:
