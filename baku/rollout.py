6792b861495c6095d52ef88e808bf836766bfd69
pianapolataa <sissi.hu@hotmail.com>
2026-01-16 08:19:40 -0500

    .

--- baku/rollout.py
+++ baku/rollout.py
@@ -504,7 +504,7 @@ class AgentRollout:
                 # Apply to Franka & Ruka
                 arm_action = self.norm_quat_vec(action[:7])
                 arm_action[2] -= 0.017
-                self.arm_action[1] -= 0.01
+                self.action[1] -= 0.01
                 arm_action[:3] = np.clip(arm_action[:3], a_min=ROBOT_WORKSPACE_MIN, a_max=ROBOT_WORKSPACE_MAX)
                 hand_action = np.clip(action[7:], self.handler.hand.min_lim, self.handler.hand.max_lim)
 
