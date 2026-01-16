ee2bfb421e370f9a2030fef789ef1eed9a573678
pianapolataa <sissi.hu@hotmail.com>
2026-01-16 11:23:27 -0500

    .

--- baku/rollout.py
+++ baku/rollout.py
@@ -241,7 +241,7 @@ from omegaconf import DictConfig
 @hydra.main(config_path="/home_shared/grail_sissi/BAKU/baku/cfgs", config_name="config")
 def main(cfg: DictConfig):
     demo_data_path = "/home_shared/grail_sissi/BAKU/processed_data_pkl/demo_task.pkl"
-    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2026.01.16_train/deterministic/105856/snapshot/43000.pt" # bread pick 2
+    snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2026.01.16_train/deterministic/105856/snapshot/47000.pt" # bread pick 2
     # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2026.01.14_train/deterministic/222512/snapshot/43000.pt" #working bread pick 1
     # snapshot_path = "/home_shared/grail_sissi/BAKU/baku/exp_local/2025.12.25_train/deterministic/105414/snapshot/43000.pt" # working music box
 
